c79fe4946936b35b59a0dc77ea3b11034d2648da
yammybajwa <92578770+yammybajwa@users.noreply.github.com>
2021-12-29 07:09:57 -0800

    add review in js file

---
+++ script.js
@@ -0,0 +1,20 @@
+const Review = [
+    {
+        id:1,
+        name:"Capriocn",
+        img:"https://images.unsplash.com/photo-1551024739-78e9d60c45ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Z2lybHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        info:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, velit dignissimos alias adipisci cupiditate expedita consequuntur voluptatibus maxime modi quasi eos. Minima, voluptates atque unde quasi fuga ullam officiis nisi.",
+    },
+    {
+        id:2,
+        name:"Jhon",
+        img:"https://images.unsplash.com/photo-1554230505-919a13968970?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lybHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        info:" Delectus, velit dignissimos alias adipisci cupiditate expedita consequuntur voluptatibus maxime modi quasi eos. Minima, voluptates atque unde quasi fuga ullam officiis nisi.",
+    },
+    {
+        id:3,
+        name:"Baby Rexha",
+        img:"./Images/bebe-rexha-singer-women-blonde-hd-wallpaper-preview (2).jpg",
+        info:"English Singeradipisci cupiditate expedita consequuntur voluptatibus maxime modi quasi eos. Minima, voluptates atque unde quasi fuga ullam officiis nisi."
+    }
+];
